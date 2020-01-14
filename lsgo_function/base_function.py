0fc607d54256a24d6a6a06b74547d37535e69717
yang.liu12 <76164587@qq.com>
2020-01-14 20:10:30 +0800

    添加LSGO测试函数的参数

--- lsgo_function/base_function.py
+++ lsgo_function/base_function.py
@@ -2,47 +2,62 @@ import numpy as np
 
 
 def sphere(x):
-    x = np.array(x)
-    return np.sum(x ** 2)
+    return np.dot(x, x)
 
 
 def dlliptic(x):
-    x = np.array(x)
-    Dim = x.size
-    f = 0
-    for i in range(Dim):
-        temp = 10 ** (6 * i / (Dim - 1))
-        f = f + temp * x[i] ** 2
-    return f
+    f = 1e6 ** np.linspace(0, 1, x.size)
+    return np.dot(f, x ** 2)
 
 
 def rastrigin(x):
-    x = np.array(x)
-    Dim = x.size
-    f = x ** 2 - 10 * np.cos(2 * np.pi * x) + 10
-    return np.sum(f)
+    return np.dot(x, x) + 10 * x.size - 10 * np.cos(2 * np.pi * x).sum()
 
 
 def ackley(x):
-    x = np.array(x)
-    Dim = x.size
-    f = -20 * np.exp(-0.2 * np.sqrt(1 / Dim * np.sum(x))) - np.exp(1 / Dim * np.sum(np.cos(2 * np.pi * x))) + 20 + np.e
-    return f
+    return -20 * np.exp(-0.2 * np.sqrt(np.dot(x, x) / x.size)) - np.exp((np.cos(2 * np.pi * x).sum()) / x.size) + 20
 
 
 def schwefel(x):
-    x = np.array(x)
-    Dim = x.size
     f = 0
-    for i in range(Dim):
-        f = f + (np.sum(x[:i + 1])) ** 2
+    for i in range(x.size):
+        f = f + (np.sum(x[0:i + 1])) ** 2
     return f
 
 
 def rosenbrock(x):
-    x = np.array(x)
-    Dim = x.size
-    f = 0
-    for i in range(Dim - 1):
-        f = f + 100 * (x[i] ** 2 - x[i + 1]) ** 2 + (x[i] - 1) ** 2
-    return f
+    a = x[0:x.size - 1] ** 2 - x[1:x.size]
+    b = x - 1
+    return 100 * np.dot(a, a) + np.dot(b, b)
+
+
+def t_asy(x, beta):
+    indx = (x > 0)
+    power = 1 + beta * (np.linspace(0, 1, x.size)[indx]) * np.sqrt(x[indx])
+    x_asy = x.copy()
+    x_asy[indx] = x[indx] ** power
+    return x_asy
+
+def _t_osz(x):
+    indx = (x > 0)
+    x_osz = x.copy()
+    x_osz[indx] = np.log(x_osz[indx])
+    x_osz[indx] = np.exp(x_osz[indx] + 0.049 * (np.sin(10 * x_osz[indx]) + np.sin(7.9 * x_osz[indx])))
+    indx = (x < 0)
+    x_osz[indx] = np.log(-1 * x_osz[indx])
+    x_osz[indx] = -1 * np.exp(x_osz[indx] + 0.049 * (np.sin(5.5 * x_osz[indx]) + np.sin(3.1 * x_osz[indx])))
+    return x_osz
+
+
+def t_diag(x, alpha):
+    temp = alpha ** (0.5 * np.linspace(0, 1, x.size))
+    return np.diag(temp, 0)
+
+
+def shift(x, x_opt):
+    return x - x_opt
+
+
+def f1(x)
+
+
---
+++ lsgo_function/cec2013_benchmark.py
@@ -0,0 +1,2 @@
+import numpy as np
+import base_function
